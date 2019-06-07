3f9a4cb20f4803a584c255b35545dcc8bdc03018
Rot-M <rtragash13@gmail.com>
2019-06-07 10:47:09 +0300

    This branch includes vgg16-in ordert to run it in google colab

--- one_shot_classification.py
+++ one_shot_classification.py
@@ -17,28 +17,22 @@ X_test, y_test = ds.preprocess_data(jpg_path, df_test, "Test")
 
 siamese_model = siamese_net.create_siamese_model(X_train)
 
-# optimizer = tf.optimizers.SGD(  #tf.keras.optimizers.Adam
-#         learning_rate=1e-4, # TODO layer wise learning_rates,
-#         decay=0.99,
-#         momentum=0.5, # TODO changing momentum
-#         name='Momentum'
-#     )
+def contrastive_loss(y_true, y_pred):
+    margin = 1
+    y_true = -1 * y_true + 1
+    return tf.keras.backend.mean((y_true) * tf.keras.backend.square(y_pred) + (1-y_true) *  tf.keras.backend.square(tf.keras.backend.maximum(margin - y_pred, 0.0)))
 
-
-optimizer = tf.optimizers.Nadam( learning_rate=0.001,
-    beta_1=0.9,
-    beta_2=0.99,
-    epsilon=1e-07,
-    name='Nadam'
-)
-
-siamese_model.compile(optimizer=optimizer,
-               loss='binary_crossentropy',
-               metrics=['accuracy'])
+optimizer = tf.optimizers.Adam (learning_rate=0.0005, decay=0.0,
+                                     beta_1=0.9,
+                                     beta_2=0.999,
+                                     epsilon=1e-08,
+                                     name='adam'
+                                     )
+siamese_model.compile (optimizer=optimizer, loss=contrastive_loss, metrics=['accuracy'])#new_model.compile(optimizer='adam', loss='categorical_crossentropy',  metrics=['accuracy'])
 
 iterations_num = 200
-batch_num = 128
-siamese_net.train_model_net(X_train, y_train, X_val, y_val, iterations_num, batch_num, list_same_train, list_diff_train, siamese_model)# insert X and YYYYYYYYYYYY
+batch_num = 32
+siamese_net.train_model_net(X_train, y_train, X_val, y_val, X_test, y_test, iterations_num, batch_num, list_same_train, list_diff_train, siamese_model)# insert X and YYYYYYYYYYYY
 
 #predict
 siamese_model.evaluate([X_test[0], X_test[1]], y_test)
--- siamese_net.py
+++ siamese_net.py
@@ -4,74 +4,44 @@ import random
 import numpy as np
 import math
 import pandas as pd
+# import keras
+# from keras.applications.imagenet_utils import _obtain_input_shape #need keras 2.2.0
+# from tensorflow.keras import backend
+# import tensorflow.get_default_graph
+from keras_vggface import VGGFace
+
+from tensorflow.python.framework import ops
+ops.reset_default_graph()
+
+
+# from keras.models import model_from_json
+# from keras import Model
+# Model.load_weights('vgg_face_weights.h5')
+
+
+
+
 
 def create_siamese_model(X_train):
+    X_train_pair1 = tf.image.grayscale_to_rgb(X_train[0])
+    X_train_pair2 = tf.image.grayscale_to_rgb (X_train[1])
+    X_train_2 = []
+    X_train_2.append(X_train_pair1)
+    X_train_2.append(X_train_pair2)
+    vggface = tf.keras.applications.vgg16.VGG16 () #if your image is different than 244*244, you should train your own classifier [include=false]
+    print(vggface.summary())
+    siamese_net = tf.keras.models.Sequential ()
+    for layer in vggface.layers:
+        siamese_net.add(layer)
+    siamese_net.pop()
+    for layer in siamese_net.layers:
+        layer.trainable=False
 
-    siamese_net = tf.keras.models.Sequential()
-
-    siamese_net.add(tf.keras.layers.Conv2D(filters=32, #64
-                                                 kernel_size=(10, 10),
-                                                 kernel_initializer =
-                                                    tf.keras.initializers.TruncatedNormal(mean = 0 ,stddev=1e-2),
-                                                 use_bias = True,
-                                                 bias_initializer =
-                                                    tf.keras.initializers.TruncatedNormal(mean = 0.5 ,stddev=1e-2),
-                                                 activation='relu',
-                                                 input_shape=X_train[0][0].shape,
-                                                 kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
-                                                 name='Conv_1'))
-    siamese_net.add(tf.keras.layers.MaxPool2D())
-
-    siamese_net.add(tf.keras.layers.Conv2D(filters=64, #128
-                                                 kernel_size=(7, 7),
-                                                 kernel_initializer=
-                                                    tf.keras.initializers.TruncatedNormal(mean=0, stddev=1e-2),
-                                                 use_bias=True,
-                                                 bias_initializer=
-                                                    tf.keras.initializers.TruncatedNormal(mean=0.5, stddev=1e-2),
-                                                 activation='relu',
-                                                 kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
-                                                 name='Conv_2'))
-    siamese_net.add(tf.keras.layers.MaxPool2D())
-
-    siamese_net.add(tf.keras.layers.Conv2D(filters=64, #128
-                                                 kernel_size=(4, 4),
-                                                 kernel_initializer=
-                                                    tf.keras.initializers.TruncatedNormal(mean=0, stddev=1e-2),
-                                                 use_bias=True,
-                                                 bias_initializer=
-                                                    tf.keras.initializers.TruncatedNormal(mean=0.5, stddev=1e-2),
-                                                 activation='relu',
-                                                 kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
-                                                 name='Conv_3'))
-    siamese_net.add(tf.keras.layers.MaxPool2D())
-
-    siamese_net.add(tf.keras.layers.Conv2D(filters=128, #256
-                                                 kernel_size=(4, 4),
-                                                 kernel_initializer=
-                                                    tf.keras.initializers.TruncatedNormal(mean=0, stddev=1e-2),
-                                                 use_bias=True,
-                                                 bias_initializer=
-                                                    tf.keras.initializers.TruncatedNormal(mean=0.5, stddev=1e-2),
-                                                 activation='relu',
-                                                 kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
-                                                 name='Conv_4'))
-
-    siamese_net.add(tf.keras.layers.Flatten())
-    siamese_net.add(tf.keras.layers.Dense(units=512, #4096
-                              activation='sigmoid',
-                              kernel_initializer=
-                                tf.keras.initializers.TruncatedNormal(mean=0, stddev=1e-2),
-                              use_bias=True,
-                              bias_initializer=
-                                tf.keras.initializers.TruncatedNormal(mean=0.5, stddev=2*(1e-1)),
-                              kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
-                              name='Dense_1'))
-
-    twin_1_input = tf.keras.layers.Input(X_train[0][0].shape)
-    twin_2_input = tf.keras.layers.Input(X_train[0][0].shape)
+    twin_shape = X_train_2[0][0].shape
+    twin_1_input = tf.keras.layers.Input(twin_shape)
 
     flatten_twin_1 = siamese_net(twin_1_input)
+    twin_2_input = tf.keras.layers.Input(twin_shape)
     flatten_twin_2 = siamese_net(twin_2_input)
 
     # Calc L1 distance between twins
@@ -81,18 +51,18 @@ def create_siamese_model(X_train):
     prediction = tf.keras.layers.Dense(units=1, activation='sigmoid')(l1_dist)
     model = tf.keras.models.Model(inputs=[twin_1_input, twin_2_input], outputs=prediction)
 
-
     return model
 
 
-def train_model_net(X_train, Y_train, X_val, Y_val, iterations_num, batch_num, list_same, list_diff,
+def train_model_net(X_train, Y_train, X_val, Y_val, X_test, y_test, iterations_num, batch_num, list_same, list_diff,
                         siamese_model):  # , support_set_size, final_momentum, momentum_slope, evaluate_each, model_name )
         """The function vlavulate haow many batches includes in train just that half will be from same and half will be from different
          after that we produce list of indexes for train anf produce new arrays (images and labels) which will be transferred to train on batch function
          """
         num_train_pairs = len(X_train[0])
         num_val_pairs = len(X_val[0])
-
+        X_val_3channels = [tf.image.grayscale_to_rgb(X_val[0]),tf.image.grayscale_to_rgb(X_val[1])]
+        X_test_3channels = [tf.image.grayscale_to_rgb(X_test[0]),tf.image.grayscale_to_rgb(X_test[1])]
         len_list_same = len(list_same)
         len_list_diff = len(list_diff)
         batch_loop_num_half = int(batch_num / 2)
@@ -112,11 +82,11 @@ def train_model_net(X_train, Y_train, X_val, Y_val, iterations_num, batch_num, l
                 list_batch_indexes.extend(list_same[index * batch_loop_num_half:(index + 1) * batch_loop_num_half])
                 list_batch_indexes.extend(list_diff[index * batch_loop_num_half:(index + 1) * batch_loop_num_half])
                 random.shuffle(list_batch_indexes)
-                X = [np.zeros((batch_num, 250, 250, 1)) for i in range(2)]
+                X = [np.zeros((batch_num, 250, 250, 3)) for i in range(2)]
                 Y = np.zeros((batch_num, 1))
                 for index_batch, value in enumerate(list_batch_indexes):
-                    X[0][index_batch, :, :] = X_train[0][value]
-                    X[1][index_batch, :, :] = X_train[1][value]
+                    X[0][index_batch, :, :] = tf.image.grayscale_to_rgb(X_train[0][value])
+                    X[1][index_batch, :, :] = tf.image.grayscale_to_rgb(X_train[1][value])
                     Y[index_batch] = Y_train[value]
                 image_batch = X
                 label_batch = Y
@@ -133,20 +103,23 @@ def train_model_net(X_train, Y_train, X_val, Y_val, iterations_num, batch_num, l
             list_rem_batch_indexes.extend(list_diff[-1 * rem_diff_pairs:])
             random.shuffle(list_rem_batch_indexes)
             len_reamins = len(list_rem_batch_indexes)
-            X = [np.zeros((len_reamins, 250, 250, 1)) for i in range(2)]
+            X = [np.zeros((len_reamins, 250, 250, 3)) for i in range(2)]
             Y = np.zeros((len_reamins, 1))
             for index_batch, value in enumerate(list_rem_batch_indexes):
-                X[0][index_batch, :, :] = X_train[0][value]
-                X[1][index_batch, :, :] = X_train[1][value]
+                X[0][index_batch, :, :] = tf.image.grayscale_to_rgb(X_train[0][value])
+                X[1][index_batch, :, :] = tf.image.grayscale_to_rgb(X_train[1][value])
                 Y[index_batch] = Y_train[value]
             image_batch = X
             label_batch = Y
-            train_loss, train_acc = siamese_model.train_on_batch(image_batch, label_batch)  # not in useeeeeeeeee
-            print('Train loss, Train Accuracy at epoch %s, final_batch: %s, %s' % (epoch, float(train_loss), float(train_acc)))
+            # train_loss, train_acc = siamese_model.train_on_batch(image_batch, label_batch)  # not in useeeeeeeeee
+            # print('Train loss, Train Accuracy at epoch %s, final_batch: %s, %s' % (epoch, float(train_loss), float(train_acc)))
             # evaluate for validation and check if there are 20 consecutive decrease in validation accuracy
-            val_loss, val_acc = siamese_model.evaluate([X_val[0], X_val[1]], Y_val)
+            val_loss, val_acc = siamese_model.evaluate([X_val_3channels[0], X_val_3channels[1]], Y_val)
             df_validation_results = df_validation_results.append(
                 {'epoch': epoch, 'loss': float(val_loss), 'accuracy': float(val_acc)}, ignore_index=True)
+            # if val_acc>0.6:
+                # break
+            test_loss,test_acc = siamese_model.evaluate ([X_test_3channels[0], X_test_3channels[1]], y_test)
             if epoch % 20 == 0:
                 print('Validation loss, Validation Accuracy at epoch %s: %s, %s' % (epoch, float (val_loss), float (val_acc)))
             if epoch > 0:
